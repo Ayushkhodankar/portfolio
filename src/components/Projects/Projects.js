ed5a5145ad5cc52b2188e3407b1a2cee212a21ce
Ayush Khodankar <ayushkhodankar01@gmail.com>
2022-12-22 17:47:30 +0530

    initial commit

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -56,10 +56,10 @@ function Projects() {
             <ProjectCard
               imgPath={editor}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="Hut'N Gut"
+              description="A mobile application build with Andriod Studio -Java to order pizzas. In this app the customers can order there own customized pizza according to their requirements."
+              ghLink="https://github.com/Ayushkhodankar/Hut-N-Gut-PizzaStore"
+              //demoLink="https://editor.soumya-jit.tech/"              
             />
           </Col>
 

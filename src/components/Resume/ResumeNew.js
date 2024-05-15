aa1056079360d228bd7422ebb0e425d72a2071bc
Ayush Khodankar <ayushkhodankar01@gmail.com>
2024-05-16 04:22:37 +0530

    initial commit

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import pdf from "../../Assets/../Assets/resumemain (1).pdf";
+import pdf from "../../Assets/../Assets/Ayush Khodankar.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";

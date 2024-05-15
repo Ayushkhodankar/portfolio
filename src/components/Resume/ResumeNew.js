8cd963578fc54a2811e02a4dad7242ea419198c5
Ayush Khodankar <ayushkhodankar01@gmail.com>
2024-05-16 04:23:25 +0530

    initial commit

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -8,7 +8,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://github.com/Ayushkhodankar/portfolio/blob/main/src/Assets/resumemain%20(1).pdf";
+  "https://github.com/Ayushkhodankar/portfolio/blob/main/src/Assets/Ayush%20Khodankar.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
